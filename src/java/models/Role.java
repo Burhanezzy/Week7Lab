2a9ca82d44d50bdb1d4ca847b7e7e22931ded9a7
burha <burha@Lenovo-Legion-5>
2023-03-11 22:01:48 -0700

    Added some code

--- src/java/models/Role.java
+++ src/java/models/Role.java
@@ -1,14 +1,41 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package models;
+
+import java.io.Serializable;
 
 /**
  *
- * @author burha
+ * @author Burhan
  */
-public class Role {
+public class Role implements Serializable{
+    private int roleID;
+    private String name;
+
+    public Role() {
+    }
+
+    public Role(int roleID, String name) {
+        this.roleID = roleID;
+        this.name = name;
+    }
+
+    public int getRoleID() {
+        return roleID;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setRoleID(int roleID) {
+        this.roleID = roleID;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+    
+    public String toString(){
+        return this.name;
+    }
     
 }
--- src/java/services/RoleService.java
+++ src/java/services/RoleService.java
@@ -1,14 +1,17 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package services;
+
+import dataaccess.RoleDB;
+import models.Role;
 
 /**
  *
- * @author burha
+ * @author Burhan
  */
 public class RoleService {
     
+    public Role getRole(int iD) throws Exception{
+        RoleDB roleDB = new RoleDB();
+        Role role = roleDB.getRole(iD);
+        return role;
+    }
 }
